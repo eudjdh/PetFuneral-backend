7751b5414fe9a026d339df9dd130a6dc3672ef56
eudjdh <1601226967@qq.com>
2025-03-12 21:00:36 +0800

    first commit

---
+++ src/main/java/com/tomfrank/PetFuneral/PetFuneralApplication.java
@@ -0,0 +1,13 @@
+package com.tomfrank.PetFuneral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFuneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFuneralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomfrank/PetFuneral/PetFuneralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomfrank.PetFuneral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFuneralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
