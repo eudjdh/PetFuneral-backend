f4c9aeb2effc604a426ca396a740a5c124ba281b
eudjdh <1601226967@qq.com>
2025-05-27 11:38:40 +0800

    AuthController允许跨域

--- src/main/java/com/tomfrank/PetFuneral/controller/AuthController.java
+++ src/main/java/com/tomfrank/PetFuneral/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:8080")
 public class AuthController {
 
     private final UserService userService;
