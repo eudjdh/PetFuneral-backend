3461e8a727a3b592ac1aaf6b13ba1e8729fab312
eudjdh <1601226967@qq.com>
2025-05-31 14:29:02 +0800

    完善注册、登录、获取用户个人信息功能

--- src/main/java/com/tomfrank/PetFuneral/controller/UserController.java
+++ src/main/java/com/tomfrank/PetFuneral/controller/UserController.java
@@ -2,19 +2,28 @@ package com.tomfrank.PetFuneral.controller;
 
 import com.tomfrank.PetFuneral.dto.*;
 import com.tomfrank.PetFuneral.service.UserService;
+import com.tomfrank.PetFuneral.util.JwtUtil;
 import com.tomfrank.PetFuneral.util.ResponseWrapper;
+import io.jsonwebtoken.Claims;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:8080")
 public class UserController {
-
     private final UserService userService;
-    public UserController(UserService userService) { this.userService = userService; }
+    private final JwtUtil jwtUtil; // 1. 声明jwtUtil为成员变量
+
+    public UserController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
 
     @GetMapping("/profile")
-    public ResponseWrapper<UserProfileVO> profile(
-            @RequestAttribute("userId") Integer userId) {
+    public ResponseWrapper<UserProfileVO> profile(@RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        Claims claims = jwtUtil.parseToken(token);
+        Integer userId = Integer.valueOf(claims.getSubject());
         return ResponseWrapper.ok(userService.getProfile(userId));
     }
 }
